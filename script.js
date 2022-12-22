7f1189b501d29de07275ce0facdcf5e39f4a7bd1
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:18 +0530

    init commit

--- script.js
+++ script.js
@@ -1,49 +1,25 @@
-let input = document.querySelector(".input"),
-  character = document.querySelector(".character"),
-  word = document.querySelector(".word"),
-  readingTime = document.querySelector(".reading-time"),
-  wordLimit = document.querySelector(".word-limit"),
-  WORD_LIMIT = 10;
+// VARIABLES
+const modal = document.querySelector(".modal"),
+  btn = document.querySelector(".btn");
+// close = document.querySelector(".close");
 
-input.addEventListener("keyup", characterCount);
-input.addEventListener("keyup", wordCounter);
+btn.addEventListener("click", openModal);
+// close.addEventListener("click", closeModal);
+// modal.addEventListener("click", closeModal);
 
-function characterCount() {
-  character.innerHTML = input.value.length;
+modal.addEventListener("click", (e) => {
+  const evt = e.target.classList;
+  if (evt.contains("modal") || evt.contains("close")) {
+    closeModal();
+  }
+});
+
+// OPEN MODAL
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = "block";
 }
-
-function wordCounter(e) {
-  let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
-
-  if (words) {
-    word.innerHTML = words.length;
-    wordLimit.innerHTML = WORD_LIMIT - words.length;
-  } else {
-    word.innerHTML = 0;
-  }
-
-  //   Word Limit Block of Code
-  input.addEventListener("keydown", function (e) {
-    words = input.value.match(/\b[-?(\w+)?]+\b/gi);
-    if (words) {
-      if (words.length > WORD_LIMIT - 1 && e.code !== "Backspace") {
-        e.preventDefault();
-        // console.log("Word limit reached");
-      }
-    }
-  });
-
-  //   Reading time based on 225 words/min
-  if (words) {
-    let seconds = Math.floor((words.length * 60) / 225);
-    if (seconds > 59) {
-      let minutes = Math.floor(seconds / 60);
-      seconds = seconds - minutes * 60;
-      readingTime.innerHTML = minutes + "m" + seconds + "s";
-    } else {
-      readingTime.innerHTML = seconds + "s";
-    }
-  } else {
-    readingTime.innerHTML = "0s";
-  }
+// CLOSE MODAL
+function closeModal() {
+  modal.style.display = "none";
 }
