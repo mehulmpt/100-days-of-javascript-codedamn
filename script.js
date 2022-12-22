78a6e893a71f4e978458f066416bd969e27cb445
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:14 +0530

    init commit

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-const image = document.getElementById("image");
-const statusDisplay = document.getElementById("status");
-const bgColor = document.getElementById("main");
+const hero = document.querySelector(".hero");
+const heroBoy = document.querySelector(".hero-boy");
+const vilan = document.querySelector(".vilan");
 
-function setColor() {
-    bgColor.classList.add("online");
-}
-
-async function connectionStatus() {
-  try {
-    const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time=' + (new Date()).getTime());
-    image.src = "./images/online.png";
-    setColor();
-    return fetchResult.status >= 200 && fetchResult.status < 300;
-  } catch (error) {
-    console.error(error);
-    statusDisplay.textContent = "OOPS!!! Your Internet Connection is Down.";
-    image.src = "./images/offline.png";
-    bgColor.classList.remove("online");
+const jump = () => {
+  if (hero.classList != "animate") {
+    hero.classList.add("animate");
+    vilan.style.animation = "move 1s infinite linear";
   }
-}
+  setTimeout(() => {
+    hero.classList.remove("animate");
+  }, 300);
+};
 
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Space") {
+    jump();
+  }
+});
 
-// Monitor the connection
-  setInterval(async () => {
-    const result = await connectionStatus();
-    if ( result ) {
-        statusDisplay.textContent = "You're ONLINE!!! Connection looks good.";
-        setColor(); 
-    } 
-  }, 5000);
-  
-//   Check Connection When Page Loads
-   window.addEventListener("load", async (event) => {
-     if ( connectionStatus() ) {
-    	statusDisplay.textContent = "Online"
-    } else {
-    	statusDisplay.textContent = "OFFline"
-    }
-  });
+let isAlive = setInterval(() => {
+  let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+  //   console.log(heroTop);
+  let vilanLeft = parseInt(
+    window.getComputedStyle(vilan).getPropertyValue("left")
+  );
+
+  if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
+    vilan.style.animation = "none";
+    alert("Game over. Press spacebar to start");
+  }
+}, 10);
