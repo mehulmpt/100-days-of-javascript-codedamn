9aff7dbc9ac2f55ec64e735a84d2e030e6500d8d
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-const searchForm = document.querySelector("#search-form");
-const input = document.querySelector("#search-input");
+const links = document.querySelectorAll(".nav-list li a");
 
-const speechBtnDiv = document.querySelector("#speech-btn");
-const micBtn = document.querySelector(".btn .fas");
-const instruction = document.querySelector(".instruction");
-
-const speechSynthesis = window.speechSynthesis;
-
-// Check for browser support
-if (speechSynthesis) {
-  console.log("Speech Synthesis Supported");
-
-  micBtn.addEventListener("click", speak);
-
-  function speak(e) {
-    e.preventDefault();
-    const inputValue = input.value;
-    const speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-    speech.text = inputValue;
-    speech.volume = "1";
-    speech.rate = "1";
-    speech.pitch = "1";
-    speech.voice = speechSynthesis.speak(speech);
-  }
-} else {
-  console.log("Speech Synthesis Not Supported");
-  speechBtnDiv.style.visibility = "hidden";
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
 }
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+// Sticky Header
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
