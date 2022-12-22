c9458959df3eb53251f36a250adecaa9d35f12e4
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:12 +0530

    init commit

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const userLocation = document.querySelector(".location");
-const btn = document.querySelector(".btn");
+const acc = document.querySelectorAll(".accordion");
 
-btn.addEventListener("click", () => {
-  navigator.geolocation.getCurrentPosition(showPosition);
-});
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    // this.classList.toggle("active");
+    const desc = this.nextElementSibling;
+    const allDesc = document.querySelectorAll(".desc");
+    const activeAcc = document.getElementsByClassName("accordion active");
+    // console.log(desc);
+    // console.log(desc.scrollHeight);
+    if (desc.style.maxHeight) {
+      desc.style.maxHeight = null;
+      this.classList.remove("active");
+    } else {
+      // Remove active class from all tabs
+      for (let i = 0; i < activeAcc.length; i++) {
+        activeAcc[i].classList.remove("active");
+      }
 
-function showPosition(position) {
-  userLocation.innerHTML = `
-        Latitude: ${position.coords.latitude} <br>
-        Longitude: ${position.coords.longitude}
-    `;
+      // Set the maxHeight of all desc to 0
+      for (let i = 0; i < allDesc.length; i++) {
+        allDesc[i].style.maxHeight = null;
+      }
+
+      // open the current tab clicked
+      desc.style.maxHeight = desc.scrollHeight + "px";
+      this.classList.add("active");
+    }
+  });
 }
