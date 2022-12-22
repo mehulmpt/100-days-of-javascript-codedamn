ddd417f07b99247e5e6c90bd89663d7a8d120644
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:13 +0530

    init commit

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-// BMI - kg/m2
-// Normal - 18.5 -24.9
-// Underweight - < 18.5
-// Overweight -  25 - 29.9
-// Obese -  30 and above
+const keys = document.getElementsByTagName("li");
+const result = document.querySelector("p");
+const clear = document.querySelector(".clear");
 
-const btn = document.querySelector(".btn"),
-  result = document.querySelector(".result"),
-  reset = document.querySelector(".reset");
-
-btn.addEventListener("click", calculateBMI);
-
-function calculateBMI(e) {
-  e.preventDefault();
-  let height = document.querySelector(".height").value;
-  let weight = document.querySelector(".weight").value;
-
-  console.log(height);
-
-  // Validate Input
-  if (height === "" || isNaN(height)) {
-    return (result.innerHTML = "Provide a valid Height!");
-  } else if (weight === "" || isNaN(weight)) {
-    return (result.innerHTML = "Provide a valid Height!");
+for (let i = 0; i < keys.length; i++) {
+  if (keys[i].innerHTML === "=") {
+    keys[i].addEventListener("click", calculate);
   } else {
-    height = height / 100;
-    let bmi = (weight / Math.pow(height, 2)).toFixed(2);
-    console.log(bmi);
-    //   Categorize result
-    if (bmi < 18.5) {
-      showResult(`Underweight: <span>${bmi}</span>`, "orange");
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-      showResult(`Normal: <span>${bmi}</span>`, "green");
-    } else if (bmi >= 25.0 && bmi < 29.9) {
-      showResult(`Overweight: <span>${bmi}</span>`, "blue");
-    } else {
-      showResult(`Obese: <span>${bmi}</span>`, "red");
-    }
+    keys[i].addEventListener("click", addToCurrentValue(i));
   }
-  reset.style.display = "block";
 }
 
-function showResult(val, color) {
-  result.style.backgroundColor = color;
-  return (result.innerHTML = val);
+function addToCurrentValue(i) {
+  return () => {
+    if (keys[i].innerHTML === "÷") {
+      result.innerHTML += "/";
+    } else if (keys[i].innerHTML === "x") {
+      result.innerHTML += "*";
+    } else {
+      result.innerHTML += keys[i].innerHTML;
+    }
+  };
 }
 
-reset.addEventListener("click", () => {
-  document.querySelector("form").reset();
-  reset.style.display = "none";
-  result.style.display = "none";
+function calculate() {
+  return (result.innerHTML = eval(result.innerHTML));
+}
+
+clear.addEventListener("click", () => {
+  result.innerHTML = "";
 });
