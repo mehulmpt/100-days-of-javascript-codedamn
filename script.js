5dbd728238a36bdce55e01e2e77122e6480f0e1c
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:13 +0530

    init commit

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-const input = document.querySelector("textarea");
 const btn = document.querySelector(".btn");
-const limit = document.querySelector(".limit");
-let max = 50;
+const coupon = document.querySelector(".coupon");
 
-const updateLimit = () => {
-  limit.textContent = max;
-  input.addEventListener("input", () => {
-    let inputLength = input.value.length;
-    limit.textContent = max - inputLength;
-    if (inputLength > max) {
-      btn.disabled = true;
-      limit.style.color = "red";
-    } else {
-      btn.disabled = false;
-      limit.style.color = "black";
-    }
-  });
-};
-
-updateLimit();
-
-// Tweet Function
-btn.addEventListener("click", (e) => {
+const copyText = (e) => {
   e.preventDefault();
-  tweet();
-});
 
-const tweet = () => {
-  const tweetInput = "https://twitter.com/intent/tweet?text=";
-  window.open(`${tweetInput}${input.value}`);
+  coupon.select();
+  coupon.setSelectionRange(0, 999999);
+  document.execCommand("copy");
+
+  btn.textContent = "Copied!!!";
+  setTimeout(() => {
+    btn.textContent = "Copy";
+  }, 3000);
 };
+
+btn.addEventListener("click", copyText);
