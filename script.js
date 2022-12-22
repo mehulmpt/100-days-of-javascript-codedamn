f3c9031ccfa37c88621fd391321f629e99d760cc
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-const tabLinks = document.getElementsByClassName("tab-link");
-const allContent = document.querySelectorAll(".tab-content");
+const celsius = document.querySelector(".celsius"),
+  fah = document.querySelector(".fahrenheit"),
+  kelvin = document.querySelector(".kelvin"),
+  form = document.querySelector("form");
 
-for (let i = 0; i < tabLinks.length; i++) {
-  tabLinks[i].addEventListener("click", function (e) {
-    const current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+form.addEventListener("input", convertWeight);
 
-    // Switch content
-    const filter = e.target.dataset.filter;
-    console.log(filter);
+function convertWeight(e) {
+  if (e.target.classList.contains("celsius")) {
+    let x = e.target.value;
+    fah.value = x * 1.8 + 32;
+    kelvin.value = x + 273.15;
+  }
 
-    allContent.forEach((content) => {
-      if (content.classList.contains(filter)) {
-        content.style.display = "block";
-      } else {
-        content.style.display = "none";
-      }
-    });
-  });
+  if (e.target.classList.contains("fahrenheit")) {
+    let x = e.target.value;
+    celsius.value = (x - 32) / 1.8;
+    kelvin.value = (x - 32) / 1.8 + 273.15;
+  }
+
+  if (e.target.classList.contains("kelvin")) {
+    let x = e.target.value;
+    fah.value = (x - 273.15) * 1.8 + 32;
+    celsius.value = parseFloat(x) - 273.15;
+  }
 }
+
+// --Convert Celsius to--
+// fah = (x * 1.8) + 32;
+// kelvin = x + 273.15;
+
+// -- Convert Fah to --
+// celsius = (x - 32) / 1.8;
+// kelvin = (x - 32) / 1.8 + 273.15;
+
+// -- Conver Kelvin to --
+// fah = (x - 273.15) * 1.8 + 32;
+// celsius = parseFloat(x) - 273.15;
