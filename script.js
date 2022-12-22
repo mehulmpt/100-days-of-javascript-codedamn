282db0c16c513ab131c16258069108859e6bfad4
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-// Variables
-const input = document.querySelector("input"),
-  btn = document.querySelector("button"),
-  todoList = document.querySelector(".todo-list"),
-  clear = document.querySelector(".clear");
+const input = document.querySelector("textarea");
+const btn = document.querySelector(".btn");
+const limit = document.querySelector(".limit");
+let max = 50;
 
-//   ADD LIST ITEM
-const addTask = (e) => {
-  e.preventDefault();
-  const newLi = document.createElement("li");
-  const delBtn = document.createElement("button");
-
-  delBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-
-  if (input.value !== "") {
-    newLi.textContent = input.value;
-    newLi.appendChild(delBtn);
-    todoList.appendChild(newLi);
-    input.value = "";
-  } else {
-    alert("Please enter a task");
-  }
-
-  //   DELETE FUNCTION
-  delBtn.addEventListener("click", function () {
-    const del = confirm("You are about to delete this task!!!");
-    if (del == true) {
-      const parent = this.parentNode;
-      parent.remove();
+const updateLimit = () => {
+  limit.textContent = max;
+  input.addEventListener("input", () => {
+    let inputLength = input.value.length;
+    limit.textContent = max - inputLength;
+    if (inputLength > max) {
+      btn.disabled = true;
+      limit.style.color = "red";
+    } else {
+      btn.disabled = false;
+      limit.style.color = "black";
     }
   });
 };
 
-btn.addEventListener("click", addTask);
-
-clear.addEventListener("click", () => {
-  todoList.innerHTML = "";
-});
+updateLimit();
