0bd92423b57fbd564d82724bf13b6cec72c921dc
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:16 +0530

    init commit

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const links = document.querySelectorAll(".nav-list li a");
+const searchForm = document.querySelector("#search-form");
+const input = document.querySelector("#search-input");
 
-for (link of links) {
-  link.addEventListener("click", smoothScroll);
-}
+const speechBtnDiv = document.querySelector("#speech-btn");
+const micBtn = document.querySelector(".btn .fas");
+const instruction = document.querySelector(".instruction");
 
-function smoothScroll(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth",
-  });
+const speechSynthesis = window.speechSynthesis;
+
+// Check for browser support
+if (speechSynthesis) {
+  console.log("Speech Synthesis Supported");
+
+  micBtn.addEventListener("click", speak);
+
+  function speak(e) {
+    e.preventDefault();
+    const inputValue = input.value;
+    const speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = inputValue;
+    speech.volume = "1";
+    speech.rate = "1";
+    speech.pitch = "1";
+    speech.voice = speechSynthesis.speak(speech);
+  }
+} else {
+  console.log("Speech Synthesis Not Supported");
+  speechBtnDiv.style.visibility = "hidden";
 }
