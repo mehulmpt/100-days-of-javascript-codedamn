efc232b64cac4a151585976a83189ad2172336ec
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-const word = document.querySelector(".input-text");
+const api = {
+    key: "28fd15358cdecbc1a1dfef367e71acef",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
-const result = document.querySelector(".result");
+btn.addEventListener("click", getInput);
 
-btn.addEventListener("click", countVowel);
-
-function countVowel() {
-  let vowelCount = 0;
-  let wordVal = word.value.toLowerCase();
-
-  for (let i = 0; i < wordVal.length; i++) {
-    let letter = wordVal[i];
-    if (letter.match(/([a,e,i,o,u])/)) {
-      vowelCount++;
+function getInput (event) {
+    event.preventDefault();
+    if (event.type == "click") {
+        getData(search.value);
+        console.log(search.value);
     }
-  }
-  result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`;
+}
+
+function getData () {
+    fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
+        .then(response => {
+            return response.json();
+        }).then(displayData);
+        
+}
+
+function displayData (response) {
+    // console.log(response);
+    if (response.cod === "404") {
+        const error = document.querySelector(".error");
+        error.textContent = "Please enter a valid city";
+        search.value = "";
+    } else {
+        const city = document.querySelector(".city");
+        city.innerText = `${response.name}, ${response.sys.country}`;
+
+        const today = new Date();
+        const date = document.querySelector(".date");
+        date.innerText = dateFunction(today);
+
+        const temp = document.querySelector(".temp");
+        temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
+
+        const weather = document.querySelector(".weather");
+        weather.innerText = `Weather: ${response.weather[0].main}`;
+
+        const tempRange = document.querySelector(".temp-range");
+        tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C / ${Math.round(response.main.temp_max)}°C`;
+
+        const weatherIcon = document.querySelector(".weather-icon");
+        const iconURL = "http://openweathermap.org/img/w/";
+        weatherIcon.src = iconURL + response.weather[0].icon + ".png";
+
+        search.value = "";
+    }
+}
+
+function dateFunction (d) {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`;
 }
