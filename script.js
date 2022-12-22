7f31f8a02c6debe136d6a2a8c1f2dcba5f45fdd0
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:14 +0530

    init commit

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const galleryFilter = document.querySelector(".gallery-filter");
-const galleryImages = document.querySelectorAll(".image");
+const fab = document.getElementById("fab");
+const fabBtns = document.querySelector(".fab-btns");
 
-galleryFilter.addEventListener("click", (e) => {
-  if (e.target.classList.contains("filter-gallery")) {
-    galleryFilter.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
-
-    const filter = e.target.dataset.filter;
-    galleryImages.forEach((image) => {
-      if (filter === "all" || image.classList.contains(filter)) {
-        image.style.display = "block";
-      } else {
-        image.style.display = "none";
-      }
-    });
-  }
+fab.addEventListener("click", () => {
+  fabBtns.classList.toggle("show");
 });
