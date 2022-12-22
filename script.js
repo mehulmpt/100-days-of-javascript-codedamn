aa7db62f875b85688d9cff122439a9967124429c
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:13 +0530

    init commit

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-// http://api.icndb.com/jokes/random
+// BMI - kg/m2
+// Normal - 18.5 -24.9
+// Underweight - < 18.5
+// Overweight -  25 - 29.9
+// Obese -  30 and above
 
-const joke = document.querySelector(".joke");
-const btn = document.querySelector(".btn");
-const url = "http://api.icndb.com/jokes/random";
+const btn = document.querySelector(".btn"),
+  result = document.querySelector(".result"),
+  reset = document.querySelector(".reset");
 
-btn.addEventListener("click", getJoke);
+btn.addEventListener("click", calculateBMI);
 
-// function getJoke() {
-//   fetch(url)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       joke.innerHTML = data.value.joke;
-//     });
-// }
+function calculateBMI(e) {
+  e.preventDefault();
+  let height = document.querySelector(".height").value;
+  let weight = document.querySelector(".weight").value;
 
-async function getJoke() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    joke.innerHTML = data.value.joke;
-    //   console.log(data);
-  } catch (error) {
-    joke.innerHTML = error.message;
+  console.log(height);
+
+  // Validate Input
+  if (height === "" || isNaN(height)) {
+    return (result.innerHTML = "Provide a valid Height!");
+  } else if (weight === "" || isNaN(weight)) {
+    return (result.innerHTML = "Provide a valid Height!");
+  } else {
+    height = height / 100;
+    let bmi = (weight / Math.pow(height, 2)).toFixed(2);
+    console.log(bmi);
+    //   Categorize result
+    if (bmi < 18.5) {
+      showResult(`Underweight: <span>${bmi}</span>`, "orange");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      showResult(`Normal: <span>${bmi}</span>`, "green");
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+      showResult(`Overweight: <span>${bmi}</span>`, "blue");
+    } else {
+      showResult(`Obese: <span>${bmi}</span>`, "red");
+    }
   }
+  reset.style.display = "block";
 }
+
+function showResult(val, color) {
+  result.style.backgroundColor = color;
+  return (result.innerHTML = val);
+}
+
+reset.addEventListener("click", () => {
+  document.querySelector("form").reset();
+  reset.style.display = "none";
+  result.style.display = "none";
+});
