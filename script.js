2feea887ffa745d84d85badb402de601e60467e7
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:16 +0530

    init commit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const hex = document.querySelector(".hex");
+const number = document.querySelector(".number");
 const btn = document.querySelector(".generate");
 
-const generateColor = () => {
-  const randomColor = Math.random().toString(16).substring(2, 8);
-  document.body.style.backgroundColor = "#" + randomColor;
-  hex.innerHTML = "#" + randomColor;
+const generateNumber = () => {
+  // Generate number btn 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = randomNumber;
 };
 
-btn.addEventListener("click", generateColor);
+btn.addEventListener("click", generateNumber);
 
-generateColor();
+generateNumber();
 
-// let color = Math.random();
-// color = Math.random().toString(16);
-// color = Math.random().toString(16).substring(2, 8);
-
-// console.log(color);
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
