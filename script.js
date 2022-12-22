120670412b27debc1a8b6318f6c185f0b7383de1
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:16 +0530

    init commit

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-const checkBox = document.querySelector("#checkbox");
-const slider = document.querySelector(".slider");
-const basic = document.querySelector(".basic");
-const prof = document.querySelector(".prof");
-const master = document.querySelector(".master");
+const btns = document.querySelectorAll(".btn");
+const storeProducts = document.querySelectorAll(".store-product");
 
-slider.addEventListener("click", () => {
-  if (!checkBox.checked) {
-    [basic.textContent, prof.textContent, master.textContent] = [
-      "120",
-      "240",
-      "360",
-    ];
-  } else {
-    [basic.textContent, prof.textContent, master.textContent] = [
-      "9.99",
-      "19.99",
-      "29.99",
-    ];
-  }
-});
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function (e) {
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+    // Switch Tab content
+    const filter = e.target.dataset.filter;
+    // console.log(filter);
+    storeProducts.forEach((product) => {
+      if (filter === "all") {
+        product.style.display = "block";
+      } else if (product.classList.contains(filter)) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  });
+}
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h2");
+const noResult = document.querySelector(".no-result");
+
+search.addEventListener("keyup", filterProducts);
+
+function filterProducts(e) {
+  const text = e.target.value.toLowerCase();
+
+  productName.forEach((product) => {
+    const item = product.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      product.parentElement.parentElement.style.display = "block";
+      noResult.style.display = "none";
+    } else {
+      product.parentElement.parentElement.style.display = "none";
+      noResult.style.display = "block";
+    }
+  });
+}
