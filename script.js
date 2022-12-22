48285d1bb7283a6d0eabf600e85c54cc13280de7
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,64 +1,18 @@
-const typedWord = document.querySelector(".typed-word");
-const cursor = document.querySelector(".cursor");
+const menu = document.querySelector(".menu");
+const avatar = document.querySelector(".avatar-profile img");
 
-const wordArray = ["Web Developer", "Product Designer", "Tech Advocate"];
-let wordArrayIndex = 0;
-let letterIndex = 0;
-
-const typingDelay = 200;
-const erasingDelay = 100;
-const newWordDelay = 2000;
-
-// Typing Function
-function type() {
-  if (letterIndex < wordArray[wordArrayIndex].length) {
-    if (!cursor.classList.contains("typing")) {
-      cursor.classList.add("typing");
-    }
-
-    typedWord.textContent += wordArray[wordArrayIndex].charAt(letterIndex);
-    letterIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursor.classList.remove("typing");
-    setTimeout(erase, newWordDelay);
-  }
-}
-
-// Erase Function
-function erase() {
-  if (letterIndex > 0) {
-    if (!cursor.classList.contains("typing")) {
-      cursor.classList.add("typing");
-    }
-    typedWord.textContent = wordArray[wordArrayIndex].substring(
-      0,
-      letterIndex - 1
-    );
-    letterIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursor.classList.remove("typing");
-    wordArrayIndex++;
-    if (wordArrayIndex >= wordArray.length) {
-      wordArrayIndex = 0;
-    }
-    setTimeout(type, typingDelay);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(type, newWordDelay);
+avatar.addEventListener("mouseover", () => {
+  menu.classList.add("active");
 });
 
-// wordArray.forEach((word, index) => {
-//   console.log(`${index} => ${word}`);
-// });
+avatar.addEventListener("mouseout", () => {
+  menu.classList.remove("active");
+});
 
-// const word = "Developer";
-// const result = word.charAt(0);
-// console.log(result);
+menu.addEventListener("mouseover", () => {
+  menu.classList.add("active");
+});
 
-// const word = "Developer";
-// const result = word.substring(0, 3);
-// console.log(result);
+menu.addEventListener("mouseout", () => {
+  menu.classList.remove("active");
+});
