c8871a49e37803e4f8f73ada273ab350b755d2c9
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:13 +0530

    init commit

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-const counters = document.querySelectorAll(".counter");
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
 
-counters.forEach((counter) => {
-  counter.innerText = 0;
-
-  function updateCounter() {
-    const target = +counter.dataset.target;
-    const count = +counter.innerText;
-    const increment = target / 200; //Count timing
-
-    if (count < target) {
-      counter.innerText = `${Math.ceil(count + increment)}`;
-      setTimeout(updateCounter, 10);
-    } else {
-      counter.innerText = target;
-    }
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+    setColor();
   }
-  //   updateCounter();
-  window.addEventListener("scroll", () => {
-    const scrollHeight = window.pageYOffset;
-    const sectionTop = document.querySelector(".top");
-    const sectionTopHeight = sectionTop.clientHeight;
-    console.log(sectionTopHeight);
-    if (scrollHeight >= sectionTopHeight - 1) {
-      updateCounter();
-    }
-  });
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+    setColor();
+  }
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+    setColor();
+  }
 });
+
+function setColor() {
+  if (count.innerHTML > 0) {
+    count.style.color = "yellow";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "orangered";
+  } else {
+    count.style.color = "#fff";
+  }
+}
+
+// add.addEventListener("click", () => {
+//   count.innerHTML++;
+// });
+// sub.addEventListener("click", () => {
+//   count.innerHTML--;
+// });
+// resetCount.addEventListener("click", () => {
+//   count.innerHTML = 0;
+// });
