688e67b0053180b89a65a19229aee8fd1d9bd241
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const menu = document.querySelector(".menu");
-const avatar = document.querySelector(".avatar-profile img");
+(function () {
+  const items = document.querySelectorAll(".timeline li");
 
-avatar.addEventListener("mouseover", () => {
-  menu.classList.add("active");
-});
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-avatar.addEventListener("mouseout", () => {
-  menu.classList.remove("active");
-});
+  function slideIn() {
+    for (let i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("slide-in");
+      } else {
+        items[i].classList.remove("slide-in");
+      }
+    }
+  }
 
-menu.addEventListener("mouseover", () => {
-  menu.classList.add("active");
-});
-
-menu.addEventListener("mouseout", () => {
-  menu.classList.remove("active");
-});
+  window.addEventListener("load", slideIn);
+  window.addEventListener("scroll", slideIn);
+  window.addEventListener("resize", slideIn);
+})();
