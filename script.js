2d864f884ee862a30244cb95d04629dfb9f90b39
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:15 +0530

    init commit

--- script.js
+++ script.js
@@ -1,52 +1,236 @@
-const formItems = document.getElementsByClassName("form-item");
-const button = document.getElementsByClassName("btn");
-const steps = document.getElementsByClassName("step");
+const play = document.querySelector(".play"),
+  previous = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  //
+  trackImage = document.querySelector(".track-image"),
+  title = document.querySelector(".title"),
+  artist = document.querySelector(".artist"),
+  //
+  trackCurrentTime = document.querySelector(".current-time"),
+  trackDuration = document.querySelector(".duration-time"),
+  slider = document.querySelector(".duration-slider"),
+  //
+  showVolume = document.querySelector("#show-volume"),
+  volumeIcon = document.querySelector("#volume-icon"),
+  currentVolume = document.querySelector("#volume"),
+  //
+  autoPlayBtn = document.querySelector(".play-all"),
+  //
+  hamBurger = document.querySelector(".fa-bars"),
+  closeIcon = document.querySelector(".fa-times"),
+  //
+  musicPlaylist = document.querySelector(".music-playlist"),
+  pDiv = document.querySelector(".playlist-div"),
+  playList = document.querySelector(".playlist");
 
-let currentFormItem = 0;
-formItems[currentFormItem].style.display = "block";
+let timer;
+let autoplay = 0;
+let indexTrack = 0;
+let songIsPlaying = false;
+let track = document.createElement("audio");
 
-if (currentFormItem == 0) {
-  button[0].style.display = "none";
-  steps[0].style.backgroundColor = "#1e9df7";
+//   All Event Listeners
+play.addEventListener("click", justPlay);
+next.addEventListener("click", nextSong);
+previous.addEventListener("click", prevSong);
+autoPlayBtn.addEventListener("click", autoPlayToggle);
+volumeIcon.addEventListener("click", muteSound);
+currentVolume.addEventListener("change", changeVolume);
+slider.addEventListener("change", changeDuration);
+track.addEventListener("timeupdate", songTimeUpdate);
+hamBurger.addEventListener("click", showPlayList);
+closeIcon.addEventListener("click", hidePlayList);
+
+// Load Tracks
+function loadTrack(indexTrack) {
+  clearInterval(timer);
+  resetSlider();
+
+  track.src = trackList[indexTrack].path;
+  trackImage.src = trackList[indexTrack].img;
+  title.innerHTML = trackList[indexTrack].name;
+  artist.innerHTML = trackList[indexTrack].singer;
+  track.load();
+
+  timer = setInterval(updateSlider, 1000);
+}
+loadTrack(indexTrack);
+
+// Play song or Pause song
+function justPlay() {
+  if (songIsPlaying == false) {
+    playSong();
+  } else {
+    pauseSong();
+  }
 }
 
-// Next Button
-button[1].addEventListener("click", () => {
-  currentFormItem++;
-  const prevFormItem = currentFormItem - 1;
+// Play Song
+function playSong() {
+  track.play();
+  songIsPlaying = true;
+  play.innerHTML = '<i class="fas fa-pause"></i>';
+}
 
-  if (currentFormItem > 0 && currentFormItem < 4) {
-    button[0].style.display = "inline-block";
-    formItems[currentFormItem].style.display = "block";
-    formItems[prevFormItem].style.display = "none";
-    steps[currentFormItem].style.backgroundColor = "#1e9df7";
+// Pause Song
+function pauseSong() {
+  track.pause();
+  songIsPlaying = false;
+  play.innerHTML = '<i class="fas fa-play"></i>';
+}
 
-    if (currentFormItem == 3) {
-      button[1].innerHTML = "Submit";
-    }
+// Next song
+function nextSong() {
+  if (indexTrack < trackList.length - 1) {
+    indexTrack++;
+    loadTrack(indexTrack);
+    playSong();
   } else {
-    if (currentFormItem > 3) {
-      // Validate the form
-      alert("Form Sunmitted Successfully");
+    indexTrack = 0;
+    loadTrack(indexTrack);
+    playSong();
+  }
+}
+
+// prev song
+function prevSong() {
+  if (indexTrack > 0) {
+    indexTrack--;
+    loadTrack(indexTrack);
+    playSong();
+  } else {
+    indexTrack = trackList.length - 1;
+    loadTrack(indexTrack);
+    playSong();
+  }
+}
+
+// Mute Sound
+function muteSound() {
+  track.volume = 0;
+  showVolume.innerHTML = 0;
+  currentVolume.value = 0;
+}
+
+// Change Volume
+function changeVolume() {
+  showVolume.value = currentVolume.value;
+  track.volume = currentVolume.value / 100;
+}
+
+// Change Duration
+function changeDuration() {
+  let sliderPosition = track.duration * (slider.value / 100);
+  track.currentTime = sliderPosition;
+}
+
+// Auto Play
+function autoPlayToggle() {
+  if (autoplay == 0) {
+    autoplay = 1;
+    autoPlayBtn.style.background = "#db6400";
+  } else {
+    autoplay = 0;
+    autoPlayBtn.style.background = "#ccc";
+  }
+}
+
+// Reste Slider
+function resetSlider() {
+  slider.value = 0;
+}
+
+// Update Slider
+function updateSlider() {
+  let position = 0;
+
+  if (!isNaN(track.duration)) {
+    position = track.currentTime * (100 / track.duration);
+    slider.value = position;
+  }
+
+  if (track.ended) {
+    play.innerHTML = '<i class="fas fa-play"></i>';
+    if (autoplay == 1 && indexTrack < trackList.length - 1) {
+      indexTrack++;
+      loadTrack(indexTrack);
+      playSong();
+    } else if (autoplay == 1 && indexTrack == trackList.length - 1) {
+      indexTrack = 0;
+      loadTrack(indexTrack);
+      playSong();
     }
   }
-});
+}
 
-// Prev Button
-button[0].addEventListener("click", () => {
-  if (currentFormItem > 0) {
-    currentFormItem--;
-    const nextFormItem = currentFormItem + 1;
-    formItems[currentFormItem].style.display = "block";
-    formItems[nextFormItem].style.display = "none";
-    steps[nextFormItem].style.backgroundColor = "#cfd2d4";
+// Update Current song time
+function songTimeUpdate() {
+  if (track.duration) {
+    let curmins = Math.floor(track.currentTime / 60);
+    let cursecs = Math.floor(track.currentTime - curmins * 60);
+    let durmins = Math.floor(track.duration / 60);
+    let dursecs = Math.floor(track.duration - durmins * 60);
 
-    if (currentFormItem == 0) {
-      button[0].style.display = "none";
+    if (dursecs < 10) {
+      dursecs = "0" + dursecs;
     }
-
-    if (currentFormItem < 3) {
-      button[1].innerHTML = "Next";
+    if (durmins < 10) {
+      durmins = "0" + durmins;
     }
+    if (curmins < 10) {
+      curmins = "0" + curmins;
+    }
+    if (cursecs < 10) {
+      cursecs = "0" + cursecs;
+    }
+    trackCurrentTime.innerHTML = curmins + ":" + cursecs;
+    trackDuration.innerHTML = durmins + ":" + dursecs;
+  } else {
+    trackCurrentTime.innerHTML = "00" + ":" + "00";
+    trackDuration.innerHTML = "00" + ":" + "00";
   }
-});
+}
+
+// Show PlayList
+function showPlayList() {
+  musicPlaylist.style.transform = "translateX(0)";
+}
+// Hide PlayList
+function hidePlayList() {
+  musicPlaylist.style.transform = "translateX(-100%)";
+}
+
+// Display Tracks in playlist
+let counter = 1;
+function displayTracks() {
+  for (let i = 0; i < trackList.length; i++) {
+    console.log(trackList[i].name);
+    let div = document.createElement("div");
+    div.classList.add("playlist");
+    div.innerHTML = `
+        <span class="song-index">${counter++}</span>
+        <p class="single-song">${trackList[i].name}</p>
+    `;
+    pDiv.appendChild(div);
+  }
+  playFromPlaylist();
+}
+
+displayTracks();
+
+// Play song from the playlist
+function playFromPlaylist() {
+  pDiv.addEventListener("click", (e) => {
+    if (e.target.classList.contains("single-song")) {
+      //   alert(e.target.innerHTML);
+      const indexNum = trackList.findIndex((item, index) => {
+        if (item.name === e.target.innerHTML) {
+          return true;
+        }
+      });
+      loadTrack(indexNum);
+      playSong();
+      hidePlayList();
+    }
+  });
+}
