747e9edb9c3c7113ccad77540e2dedbe39a0c50f
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:18 +0530

    init commit

--- script.js
+++ script.js
@@ -1,66 +1,49 @@
-const search = document.querySelector("input");
-const form = document.querySelector("form");
-const searchResults = document.querySelector(".results");
-const errorMsg = document.querySelector(".alert");
-const line = document.querySelector("hr");
+let input = document.querySelector(".input"),
+  character = document.querySelector(".character"),
+  word = document.querySelector(".word"),
+  readingTime = document.querySelector(".reading-time"),
+  wordLimit = document.querySelector(".word-limit"),
+  WORD_LIMIT = 10;
 
-const apiURL =
-  "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
+input.addEventListener("keyup", characterCount);
+input.addEventListener("keyup", wordCounter);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const searchValue = search.value;
-  if (searchValue === "") {
-    errorMessage("Search cannot be empty, please enter a search term.");
-  } else {
-    getResult(searchValue);
-  }
-});
-
-// Error Message
-function errorMessage(msg) {
-  errorMsg.style.display = "block";
-  line.style.display = "block";
-  errorMsg.textContent = msg;
+function characterCount() {
+  character.innerHTML = input.value.length;
 }
 
-async function getResult(searchVal) {
-  const response = await fetch(apiURL + searchVal);
-  const results = await response.json();
+function wordCounter(e) {
+  let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
 
-  console.log(results);
-  if (results.query.search.length == 0) {
-    return errorMessage("Invalid search, please enter another search term.");
+  if (words) {
+    word.innerHTML = words.length;
+    wordLimit.innerHTML = WORD_LIMIT - words.length;
   } else {
-    displayResults(results);
+    word.innerHTML = 0;
   }
-}
 
-// Display Results
-function displayResults(results) {
-  line.style.display = "block";
-  let output = "";
-  results.query.search.forEach((result) => {
-    let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
-    output += `
-    <div class="result p-2">
-    <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold"
-      >${result.title}</a
-    >
-    <br />
-    <a
-      href="${resultURL}"
-      target="_blank"
-      rel="noopener"
-      class="fs-5 text-success"
-      >${resultURL}</a
-    >
-    <p class="fs-5">
-    ${result.snippet}
-    </p>
-  </div>
-    `;
-    searchResults.innerHTML = output;
+  //   Word Limit Block of Code
+  input.addEventListener("keydown", function (e) {
+    words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+    if (words) {
+      if (words.length > WORD_LIMIT - 1 && e.code !== "Backspace") {
+        e.preventDefault();
+        // console.log("Word limit reached");
+      }
+    }
   });
+
+  //   Reading time based on 225 words/min
+  if (words) {
+    let seconds = Math.floor((words.length * 60) / 225);
+    if (seconds > 59) {
+      let minutes = Math.floor(seconds / 60);
+      seconds = seconds - minutes * 60;
+      readingTime.innerHTML = minutes + "m" + seconds + "s";
+    } else {
+      readingTime.innerHTML = seconds + "s";
+    }
+  } else {
+    readingTime.innerHTML = "0s";
+  }
 }
