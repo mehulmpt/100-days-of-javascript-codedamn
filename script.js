87e3bf6bb25452ea196995008d1042668a1ef20d
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:16 +0530

    init commit

--- script.js
+++ script.js
@@ -1,71 +1,21 @@
-const typedWord = document.querySelector(".typed-word");
-const cursor = document.querySelector(".cursor");
+const checkBox = document.querySelector("#checkbox");
+const slider = document.querySelector(".slider");
+const basic = document.querySelector(".basic");
+const prof = document.querySelector(".prof");
+const master = document.querySelector(".master");
 
-const wordArray = ["Web Developer", "Product Designer", "Tech Advocate"];
-let wordArrayIndex = 0;
-let letterIndex = 0;
-
-const typingDelay = 200;
-const erasingDelay = 100;
-const newWordDelay = 2000;
-
-// Typing Function
-function type() {
-  if (letterIndex < wordArray[wordArrayIndex].length) {
-    if (!cursor.classList.contains("typing")) {
-      cursor.classList.add("typing");
-    }
-
-    typedWord.textContent += wordArray[wordArrayIndex].charAt(letterIndex);
-    letterIndex++;
-    setTimeout(type, typingDelay);
+slider.addEventListener("click", () => {
+  if (!checkBox.checked) {
+    [basic.textContent, prof.textContent, master.textContent] = [
+      "120",
+      "240",
+      "360",
+    ];
   } else {
-    cursor.classList.remove("typing");
-    setTimeout(erase, newWordDelay);
+    [basic.textContent, prof.textContent, master.textContent] = [
+      "9.99",
+      "19.99",
+      "29.99",
+    ];
   }
-}
-
-// Erase Function
-function erase() {
-  if (letterIndex > 0) {
-    if (!cursor.classList.contains("typing")) {
-      cursor.classList.add("typing");
-    }
-    typedWord.textContent = wordArray[wordArrayIndex].substring(
-      0,
-      letterIndex - 1
-    );
-    letterIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursor.classList.remove("typing");
-    wordArrayIndex++;
-    if (wordArrayIndex >= wordArray.length) {
-      wordArrayIndex = 0;
-    }
-    setTimeout(type, typingDelay);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(type, newWordDelay);
-});
-
-// wordArray.forEach((word, index) => {
-//   console.log(`${index} => ${word}`);
-// });
-
-// const word = "Developer";
-// const result = word.charAt(0);
-// console.log(result);
-
-// const word = "Developer";
-// const result = word.substring(0, 3);
-// console.log(result);
-
-// Preloader JS
-const preloader = document.querySelector(".preloader");
-
-window.addEventListener("load", () => {
-  preloader.style.display = "none";
 });
