b4c168c3319fc6fb9b132ee1433cdf2cad95c484
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:17 +0530

    init commit

--- script.js
+++ script.js
@@ -1,67 +1,59 @@
-const api = {
-    key: "28fd15358cdecbc1a1dfef367e71acef",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
 
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", getInput);
+const speechBtnDiv = document.querySelector("#speech-btn");
+const micBtn = document.querySelector(".btn .fas");
+const instruction = document.querySelector(".instruction");
 
-function getInput (event) {
-    event.preventDefault();
-    if (event.type == "click") {
-        getData(search.value);
-        console.log(search.value);
-    }
-}
+const speechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-function getData () {
-    fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
-        .then(response => {
-            return response.json();
-        }).then(displayData);
-        
-}
+// Check for browser support
+if (speechRecognition) {
+  console.log("Speech Recognition Supported");
 
-function displayData (response) {
-    // console.log(response);
-    if (response.cod === "404") {
-        const error = document.querySelector(".error");
-        error.textContent = "Please enter a valid city";
-        search.value = "";
+  const recognition = new speechRecognition();
+
+  micBtn.addEventListener("click", micBtnClicked);
+  function micBtnClicked(e) {
+    e.preventDefault();
+    if (micBtn.classList.contains("fa-microphone")) {
+      recognition.start();
     } else {
-        const city = document.querySelector(".city");
-        city.innerText = `${response.name}, ${response.sys.country}`;
-
-        const today = new Date();
-        const date = document.querySelector(".date");
-        date.innerText = dateFunction(today);
-
-        const temp = document.querySelector(".temp");
-        temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
-
-        const weather = document.querySelector(".weather");
-        weather.innerText = `Weather: ${response.weather[0].main}`;
-
-        const tempRange = document.querySelector(".temp-range");
-        tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C / ${Math.round(response.main.temp_max)}°C`;
-
-        const weatherIcon = document.querySelector(".weather-icon");
-        const iconURL = "http://openweathermap.org/img/w/";
-        weatherIcon.src = iconURL + response.weather[0].icon + ".png";
-
-        search.value = "";
+      recognition.stop();
     }
-}
+  }
 
-function dateFunction (d) {
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  //   Start Speech Recognition
+  recognition.addEventListener("start", () => {
+    micBtn.classList.remove("fa-microphone");
+    micBtn.classList.add("fa-microphone-slash");
+    instruction.textContent = "Recording...";
+    searchInput.focus();
+    console.log("Speech Recognition Enabled");
+  });
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  //   Stop Speech Recognition
+  recognition.addEventListener("end", () => {
+    micBtn.classList.remove("fa-microphone-slash");
+    micBtn.classList.add("fa-microphone");
+    instruction.textContent = "Click the Mic icon to start";
+    searchInput.focus();
+    console.log("Speech Recognition Disabled");
+  });
 
-    return `${day}, ${date} ${month} ${year}`;
+  //   Get Result of speech Recognition
+  recognition.continuous = true;
+  let content = "";
+  recognition.addEventListener("result", (e) => {
+    console.log(e);
+    const current = e.resultIndex;
+    const transcript = e.results[current][0].transcript;
+    content += transcript;
+    searchInput.value = content;
+    searchInput.focus();
+  });
+} else {
+  console.log("Speech Recognition Not Supported");
+  speechBtnDiv.style.visibility = "hidden";
 }
