d9bb010ff0b603d69fa50fc93212d651ccaec4e9
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:16 +0530

    init commit

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
-const links = document.querySelectorAll(".nav-list li a");
+// SCROLL TO TOP
+const scrollBtn = document.querySelector(".top");
+const rootEl = document.documentElement;
 
-for (link of links) {
-  link.addEventListener("click", smoothScroll);
+document.addEventListener("scroll", showBtn);
+scrollBtn.addEventListener("click", scrollToTop);
+
+function showBtn() {
+  const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+  if (rootEl.scrollTop / scrollTotal > 0.3) {
+    scrollBtn.classList.add("show-btn");
+  } else {
+    scrollBtn.classList.remove("show-btn");
+  }
 }
 
-function smoothScroll(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  document.querySelector(href).scrollIntoView({
+function scrollToTop() {
+  rootEl.scrollTo({
+    top: 0,
     behavior: "smooth",
   });
-  hideMenu();
 }
-
-// RESPONSIVE MOBILE MENU
-const menuWrapper = document.querySelector(".nav-wrapper");
-const menu = document.querySelector(".nav-list");
-const hamburger = document.querySelector(".hamburger");
-const close = document.querySelector(".close");
-
-const showMenu = () => {
-  hamburger.style.display = "none";
-  close.style.transform = "translateY(0)";
-  menuWrapper.style.transform = "translateX(0)";
-  menu.style.transform = "translateX(0)";
-};
-
-const hideMenu = () => {
-  close.style.transform = "translateY(-20rem)";
-  hamburger.style.display = "block";
-  menuWrapper.style.transform = "translateX(-200%)";
-  menu.style.transform = "translateX(200%)";
-  subMenuThree.style.transform = "translateX(-100%)";
-};
-
-hamburger.addEventListener("click", showMenu);
-close.addEventListener("click", hideMenu);
-menuWrapper.addEventListener("click", hideMenu);
-
-// Submenu Section
-const thirdLink = document.querySelector(".third-link");
-const back = document.querySelector(".back-to-menu");
-const subMenuThree = document.querySelector(".submenu-three");
-
-thirdLink.addEventListener("click", () => {
-  menu.style.transform = "translateX(-100%)";
-  subMenuThree.style.transform = "translateX(0)";
-});
-
-back.addEventListener("click", () => {
-  menu.style.transform = "translateX(0)";
-  subMenuThree.style.transform = "translateX(-100%)";
-});
