3a3c34a8c78869f2772df363be59bbea69976d1a
Mehul Mohan <mehulmpt@gmail.com>
2022-12-22 16:40:13 +0530

    init commit

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const rangeSlider = document.querySelector("input");
-const valueEl = document.querySelector(".value");
-valueEl.textContent = rangeSlider.value;
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
 
-rangeSlider.oninput = function () {
-  valueEl.textContent = this.value;
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
 };
+
+// Disable Dark Mode
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  toggleText.textContent = "Dark";
+  localStorage.setItem("darkMode", null);
+};
+
+// Save DarkMode History
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+// Add Event Listener
+toggleDarkMode.addEventListener("click", () => {
+  let darkMode = localStorage.getItem("darkMode");
+
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
